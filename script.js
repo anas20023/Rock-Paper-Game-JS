63595b6e7ecd584f8c54c40fb929ee2759c773f3
anas20023 <yorder07@gmail.com>
2023-12-18 03:06:09 +0600

    Update game UI and logic

--- script.js
+++ script.js
@@ -13,3 +13,88 @@ light.addEventListener("click", () => {
   dark.classList.remove("hidden");
   light.classList.add("hidden");
 });
+
+/// Main Game Part Starts Here
+
+let win=loose=draw=0;
+
+let MatchResult = (userch) => {
+  if (store_Val[comch] === userch) {
+    document.querySelector("#msg").innerHTML = "Game Draw ! ";
+    draw++;
+    DrawMatch();
+  } else {
+    if (userch === "sscr") {
+      if (store_Val[comch] === "rock") {
+        document.querySelector("#msg").innerHTML = "Computer win";
+        loose++;
+        Losemsg();
+      } else {
+        document.querySelector("#msg").innerHTML = "You Win";
+        win++;
+        Winmsg();
+      }
+    } else if (userch === "paper") {
+      if (store_Val[comch] === "sscr") {
+        document.querySelector("#msg").innerHTML = "Computer win";
+        loose++;
+        Losemsg();
+      } else {
+        document.querySelector("#msg").innerHTML = "You Win";
+        win++;
+        Winmsg();
+      }
+    } else if (userch === "rock") {
+      if (store_Val[comch] === "paper") {
+        document.querySelector("#msg").innerHTML = "Computer win";
+        loose++;
+        Losemsg();
+      } else {
+        document.querySelector("#msg").innerHTML = "You Win";
+        win++;
+        Winmsg();
+      }
+    }
+  }
+};
+
+let click_User = document.querySelectorAll(".box");
+click_User.forEach((sec) => {
+  sec.addEventListener("click", () => {
+    let userch = sec.getAttribute("id");
+    let comch=Computerch();
+    MatchResult(userch);
+    Display();
+  });
+});
+
+//storing the Value in the
+let store_Val = ["sscr", "paper", "rock"];
+// Function For GenRandom Index
+let Computerch = () => {
+  return Math.floor(Math.random() * 3);
+};
+// For Messege Display Style Win
+let Winmsg = () => {
+  document.getElementById("msg").style.backgroundColor = "green";
+};
+// For Messege Display Style Loose
+let Losemsg = () => {
+  document.getElementById("msg").style.backgroundColor = "red";
+};
+// For Messege Display Style Draw
+let DrawMatch = () => {
+  document.getElementById("msg").style.backgroundColor = "#333";
+};
+
+
+/// Display Score Value
+let win_d=document.querySelector("#user_score");
+let loose_d=document.querySelector("#Computer_score");
+let draw_d=document.querySelector("#drw");
+let Display=()=>
+{
+  win_d.innerHTML=win;
+  loose_d.innerHTML=loose;
+  draw_d.innerHTML=draw;
+}
